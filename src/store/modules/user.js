4ccc7853977f67dfd0750d061bb7e889ffc9dccb
wvciaobai <wvxiaobai@gmail.com>
2018-11-27 19:32:09 +0800

    权限+语言配置

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -80,6 +80,21 @@ const user = {
         removeToken()
         resolve()
       })
+    },
+    ChangeRoles({ commit, dispatch }, role) {
+      return new Promise(resolve => {
+        commit('SET_TOKEN', role)
+        setToken(role)
+        getInfo(role).then(response => {
+          const data = response.data
+          commit('SET_ROLES', data.roles)
+          commit('SET_NAME', data.name)
+          commit('SET_AVATAR', data.avatar)
+          commit('SET_INTRODUCTION', data.introduction)
+          dispatch('GenerateRoutes', data) // 动态修改权限后 重绘侧边菜单
+          resolve()
+        })
+      })
     }
   }
 }
